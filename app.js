acac84fdfbabddd953990f14486e58f2a341c0a7
alexiscameronfrankel <alexiscameronfrankel@gmail.com>
2020-05-09 19:09:58 -0400

    small tweak

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", function(req,res){
 
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/");
   
 }); 
 
 app.post("/", function(req,res){
     const query = req.body.cityName;
     const apiKey = "37e6206f1a7f3779041c9c29b234fd8b";
-    const unit = "metric"
+    const unit = "imperial"
     const url= `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${query}&units=${unit}`
 
 https.get(url, function(response){
@@ -28,7 +28,7 @@ https.get(url, function(response){
         const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
         console.log(weatherDescription)
         res.write(`<h1>The forecast is ${weatherDescription} </h1>`)
-        res.write(`<h2>and it is ${temp} degrees</h2>`)
+        res.write(`<h2>and it is ${temp} degrees F</h2>`)
         res.write(`<img src=${iconUrl}>`);
         res.send() //you can only have one res.send in a GET request so you can do res.write and then res.send 
     })
